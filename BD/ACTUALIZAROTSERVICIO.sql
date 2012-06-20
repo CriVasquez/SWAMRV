4901e933e000f41e2e7e553068245db7cb50c0c6
Cristian Vasquez Lean <cristianvlean@gmail.com>
2012-06-20 13:28:53 -0400

    Procedimientos Almacenados Finales

--- BD/ACTUALIZAROTSERVICIO.sql
+++ BD/ACTUALIZAROTSERVICIO.sql
@@ -1,20 +1,23 @@
 --------------------------------------------------------
---  File created - miércoles-junio-20-2012   
+--  File created - martes-junio-19-2012   
 --------------------------------------------------------
 --------------------------------------------------------
---  DDL for Procedure REGISTRAROTSERVICIO
+--  DDL for Procedure ACTUALIZAROTSERVICIO
 --------------------------------------------------------
 set define off;
 
-  CREATE OR REPLACE PROCEDURE "SOPRAFAMRV"."REGISTRAROTSERVICIO" (
+  CREATE OR REPLACE PROCEDURE "SOPRAFAMRV"."ACTUALIZAROTSERVICIO" (
 v_idot orden_trabajo_servicio.id_ot%type,
 v_patente orden_trabajo_servicio.patente%type,
 v_idservicio orden_trabajo_servicio.id_servicio%type
 )
 is
 BEGIN
-INSERT INTO ORDEN_TRABAJO_SERVICIO(id_ot, patente, id_servicio) 
-VALUES (v_idot, v_patente, v_idservicio);
+
+UPDATE ORDEN_TRABAJO_SERVICIO
+set id_servicio = v_idservicio
+where id_ot = v_idot
+and patente = v_patente;
 
 commit;
 exception
@@ -25,7 +28,7 @@ WHEN NO_DATA_FOUND then
 DBMS_OUTPUT.PUT_LINE ('No se han encontrado los datos en la Base de Datos');
 
 WHEN OTHERS THEN
-RAISE_APPLICATION_ERROR (-20200, 'Ha ocurrido un error al registrar la orden '||v_idot||' del vehículo '||v_patente||'. Operacion interrumpida.');
+RAISE_APPLICATION_ERROR (-20200, 'Ha ocurrido un error al actualizar la orden '||v_idot||' del vehículo '||v_patente||'. Operacion interrumpida.');
 rollback;
 end;
 
